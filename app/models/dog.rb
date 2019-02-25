dbe19dda6c10fc3bfb95ffbeaee4189e5e09c280
Stephen Wright <scw248@gmail.com>
2019-02-25 16:33:59 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,15 @@
 class Dog 
   
+  attr_accessor :name, :breed, :age
+  
+  @@all = []
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@all << self
+  end
+  
+  
 end
